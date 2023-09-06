937e322b4be4c1a3af2dc27d69b53fd5a5853933
kunaldureja <88027418+kunaldureja@users.noreply.github.com>
2023-09-07 00:29:33 +0530

    Update depthothetree.cpp

--- depthothetree.cpp
+++ depthothetree.cpp
@@ -4,7 +4,7 @@ int height(node *p){
     if(p!=NULL){
         x = height(p->leftchild);
         y = height(p->rightchild);
-        if(x>y)
+        if(x>y)  //x count the nodes of left subtree and y count the nodes of the righ subrtree
         return x+1;
         else
         return y+1;
